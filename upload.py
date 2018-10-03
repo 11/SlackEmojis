d9ee1859a9d63ae8848fc7af9ff06428e460d7b5
doug rudolph <drudolph914@gmail.com>
2018-10-03 18:00:22 -0400

    documenting upload script

--- upload.py
+++ upload.py
@@ -1,6 +1,5 @@
 # Author: Doug Rudolph -- github.com/11
 # Created: Sep 27th, 2018
-# Description: This file is currently a work in progress
 
 import requests
 import os
@@ -17,17 +16,36 @@ class Slack:
         pass
 
 
-class Aggregator:
-
-    def collect_local_emojis(self, filepath):
-        pass
-
-    def collect_emoji_by_url(self, url):
-        pass
-
-
 class Parser:
-    pass
+    """
+    Different flags and their meanings
+
+    1.) --workspace or -w:
+        - The subdomain used when logging into slack; often the name of the slack.
+        - Example: gamedev.slack.com
+        - ie. <subdomain>.slack.com
+
+    2.) --email or -e:
+        - The email used to log into slack
+
+    3.) --password or -p:
+        - The password used to log into slack
+
+    4.) --url or -u:
+        - The url to an emoji you want uploaded
+
+    5.) --directory or -d :
+        - A relative path to a directory of emojis or images
+        - When linking to a directory, this flag will search for *.gif, *.jpg, and *.png files and ignore all other filetypes
+
+
+    Example Command:
+        slackmoji --workspace slack_name --email user@gmail.com --password abc123 --directory ./emojis/
+        slackmoji -w slack_name -e user@gmail.com -p abc123 -d ./emojis/
+
+        either command is the same
+    """
+
 
 
 if __name__ == '__main__':
